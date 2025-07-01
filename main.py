a25098aae71abc2b08498887fd25816a3d5140dd
Jamie Whitehair <jamieabw@protonmail.com>
2025-07-01 17:48:38 +0100

    Added Bogo and Insertion sorts, added speed scale.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 from bar import Bar
 from sortingAlgorithms import SortingAlgorithms
 import random
+import threading
 
 DEFAULT_BAR_COUNT = 30
 DEFAULT_HEIGHT_MULTIPLIER = 20
@@ -14,6 +15,7 @@ class App(tk.Tk):
         self.geometry("1200x800")
         self.resizable(False, False)
         self.bars = []
+        self.speed = 10
         self.barCount = DEFAULT_BAR_COUNT
         self.sortingArea = tk.Canvas(self, width=800, height=800, bg="black")
         self.sortingArea.place(relx=1, rely=0, anchor="ne")
@@ -44,23 +46,44 @@ class App(tk.Tk):
         self.barsSlider = tk.Scale(self.settingsFrame, from_=0, to=300, orient=tk.HORIZONTAL, command=self.createBars, bg="grey", bd=0, highlightthickness=0)
         self.barsSlider.set(DEFAULT_BAR_COUNT)
         self.barsSlider.place(relx=0, rely=0.02, anchor="nw", relwidth=1)
-        self.update_idletasks() # calls this to allow proper fitting, heights messed up otherwise
-        self.shuffleButton = tk.Button(self.settingsFrame, text="Shuffle", command=self.createBars, bg="grey")
-        self.shuffleButton.place(relx=0, rely=0.02 + (self.barsSlider.winfo_height() / self.settingsFrame.winfo_height()), anchor="nw", relwidth=1)
         self.sortButton = tk.Button(self.settingsFrame, text="Sort", bg="grey", command=self.startSort)
         self.sortButton.place(relx=0, rely=1, anchor="sw", relwidth=1)
+        self.update_idletasks() # calls this to allow proper fitting, heights messed up otherwise
+        self.shuffleButton = tk.Button(self.settingsFrame, text="Shuffle", command=self.createBars, bg="grey")
+        self.shuffleButton.place(relx=0, rely=1 - (self.sortButton.winfo_height() / self.settingsFrame.winfo_height()), anchor="sw", relwidth=1)
+        tk.Label(self.settingsFrame, text="Speed Of Sort:", bg="grey").place(relx=0.5, rely=0.1, anchor="center")
+        self.speedSlider = tk.Scale(self.settingsFrame, from_=1, to=1000, orient="horizontal", command=self.setSpeed, bg="grey", bd=0, highlightthickness=0)
+        self.speedSlider.place(relx=0,
+                                rely=0.08 + (self.barsSlider.winfo_height() / self.settingsFrame.winfo_height()), anchor="nw", relwidth=1)
+        
+
+        self.sortSelected = tk.StringVar()
+        self.sortSelected.set("Bubble Sort")
+        self.sortChoices = ["Bubble Sort", "Insertion Sort", "Bogo Sort", "Merge Sort", "Quick Sort"]
+        self.sortSelection = tk.OptionMenu(self.settingsFrame, self.sortSelected, *self.sortChoices)
+        self.sortSelection.config(bg="grey", bd=1, highlightthickness=0)
+        self.sortSelection.place(relx=0, rely=0.5, relwidth=1)
+        
+    def setSpeed(self, value=None):
+        self.speed = int(1000 / self.speedSlider.get())
+
 
 
     def startSort(self):
         # this is currently a placeholder, colouring and audio are not implemented, WIP
-        self.bars = SortingAlgorithms.bubbleSort(self, self.bars)
-        """for j in range(self.barCount):
-            self.sortingArea.create_rectangle(j*(self.bars[j].width), 
-                                              self.sortingArea.winfo_height() - self.bars[j].height, 
-                                              j*(self.bars[j].width) + self.bars[j].width, 
-                                              self.sortingArea.winfo_height(),
-                                            fill="white",
-                                            tags=f"{self.bars[j].height / DEFAULT_HEIGHT_MULTIPLIER}")"""
+        match self.sortSelected.get():
+            case "Bubble Sort":
+                self.bars = SortingAlgorithms.bubbleSort(self, self.bars, self.speed)
+            case "Insertion Sort":
+                self.bars = SortingAlgorithms.insertionSort(self, self.bars, self.speed)
+            case "Bogo Sort":
+                self.bars = SortingAlgorithms.bogoSort(self, self.bars, self.speed)
+            case "Merge Sort":
+                pass
+            case "Quick Sort":
+                pass
+
+        
 
 
 
--- sortingAlgorithms.py
+++ sortingAlgorithms.py
@@ -1,42 +1,38 @@
+import random
 DEFAULT_HEIGHT_MULTIPLIER = 20
 class SortingAlgorithms:
     # the drawing and colouring aspects can probably be put in their own class, WIP as currently only one sort implemented
     @staticmethod
-    def bubbleSort(root, bars):
-        sorted = False
-        while not sorted:
-            sorted = True
-            for i in range(len(bars) -1):
-                if (bars[i].height > bars[i+1].height):
-                    sorted = False
-                    temp = bars[i]
-                    bars[i] = bars[i+1]
-                    bars[i+1] = temp
-                    root.sortingArea.delete(f"{bars[i].height / DEFAULT_HEIGHT_MULTIPLIER}")
-                    root.sortingArea.delete(f"{bars[i+1].height / DEFAULT_HEIGHT_MULTIPLIER}")
-                    root.sortingArea.create_rectangle(i*(root.bars[i].width), 
-                                                        root.sortingArea.winfo_height() - root.bars[i].height, 
-                                                        i*(root.bars[i].width) + root.bars[i].width, 
-                                                        root.sortingArea.winfo_height(),
-                                                        fill="red",
-                                                        tags=(f"{root.bars[i].height / DEFAULT_HEIGHT_MULTIPLIER}", "RECT"))
-                    root.sortingArea.itemconfig("RECT", fill="white")
-                    root.sortingArea.create_rectangle((i+1)*(root.bars[(i+1)].width), 
-                                                        root.sortingArea.winfo_height() - root.bars[(i+1)].height, 
-                                                        (i+1)*(root.bars[(i+1)].width) + root.bars[(i+1)].width, 
-                                                        root.sortingArea.winfo_height(),
-                                                        fill="red",
-                                                        tags=(f"{root.bars[(i+1)].height / DEFAULT_HEIGHT_MULTIPLIER}", "RECT"))
-                root.after(10, None)
+    def bubbleSort(root, bars, speed, i=0, j=0):
+        barsLength = len(bars)
+        if i < barsLength - 1:
+            if j < barsLength - i - 1:
+                if bars[j].height > bars[j+1].height:
+                    bars[j], bars[j+1] = bars[j+1], bars[j]
+
+                root.sortingArea.delete("all")
+                for idx, bar in enumerate(bars):
+                    root.sortingArea.create_rectangle(
+                        idx * bar.width,
+                        root.sortingArea.winfo_height() - bar.height,
+                        idx * bar.width + bar.width,
+                        root.sortingArea.winfo_height(),
+                        fill="red" if idx == j or idx == j+1 else "white",
+                        tags=(f"{bar.height / DEFAULT_HEIGHT_MULTIPLIER}", "RECT")
+                    )
+
+                root.after(speed, lambda: SortingAlgorithms.bubbleSort(root, bars, speed, i, j + 1))
+            else:
+                root.after(speed, lambda: SortingAlgorithms.bubbleSort(root, bars, speed, i + 1, 0))
+        else:
+            for i in range(len(bars)):  
+                root.sortingArea.itemconfig(f"{bars[i].height / DEFAULT_HEIGHT_MULTIPLIER}", fill="green")
                 root.sortingArea.update()
-        root.sortingArea.itemconfig("RECT", fill="white")
-        for bar in bars:
-            root.sortingArea.itemconfig(f"{bar.height / DEFAULT_HEIGHT_MULTIPLIER}", fill="green")
-            root.sortingArea.update()
-            root.after(10, None)
-        root.after(1000, None)
-        root.sortingArea.itemconfig("RECT", fill="white")
+                root.after(10)
+            root.after(1000)
+            root.sortingArea.itemconfig("RECT", fill="white")
         return bars
+
             
 
 
@@ -45,14 +41,85 @@ class SortingAlgorithms:
         pass
     
     @staticmethod
-    def insertionSort():
-        pass
+    def insertionSort(root, bars, speed, i=1, j=None):
+        if i < len(bars):
+            if j is None:
+                j = i - 1
+                key = bars[i]
+                SortingAlgorithms.insertionSort(root, bars, speed, i, j)
+                return
+
+            if j >= 0 and bars[j].height > bars[j + 1].height:
+                bars[j + 1], bars[j] = bars[j], bars[j + 1]
+                root.sortingArea.delete("all")
+                for idx, bar in enumerate(bars):
+                    root.sortingArea.create_rectangle(
+                        idx * bar.width,
+                        root.sortingArea.winfo_height() - bar.height,
+                        idx * bar.width + bar.width,
+                        root.sortingArea.winfo_height(),
+                        tags=(f"{bar.height / DEFAULT_HEIGHT_MULTIPLIER}", "RECT"),
+                        fill="white"
+                    )
+                root.sortingArea.itemconfig(f"{bars[j].height / DEFAULT_HEIGHT_MULTIPLIER}", fill="red")
+                root.sortingArea.itemconfig(f"{bars[j+1].height / DEFAULT_HEIGHT_MULTIPLIER}", fill="red")
+
+                root.sortingArea.update()
+                root.after(speed, SortingAlgorithms.insertionSort, root, bars, speed, i, j - 1)
+            else:
+                root.after(speed, SortingAlgorithms.insertionSort, root, bars, speed, i + 1)
+        else:
+            # Final coloring
+            for bar in bars:
+                root.sortingArea.itemconfig(f"{bar.height / DEFAULT_HEIGHT_MULTIPLIER}", fill="green")
+                root.sortingArea.update()
+                root.after(10)
+            root.after(1000)
+            root.sortingArea.itemconfig("RECT", fill="white")
+            return bars
+        
+    
+
+
+        
+        
 
     @staticmethod
     def quickSort():
         pass
 
     @staticmethod
-    def randomSort():
-        pass
+    def bogoSort(root, bars, speed):
+        sorted = False
+        while not sorted:
+            random.shuffle(bars)
+            root.sortingArea.delete("all")
+            for idx, bar in enumerate(bars):
+                root.sortingArea.create_rectangle(
+                idx * bar.width,
+                root.sortingArea.winfo_height() - bar.height,
+                idx * bar.width + bar.width,
+                root.sortingArea.winfo_height(),
+                tags=(f"{bar.height / DEFAULT_HEIGHT_MULTIPLIER}", "RECT")
+                    )
+            root.sortingArea.itemconfig("RECT", fill="red")
+            root.sortingArea.update()
+            root.after(speed)
+            root.sortingArea.itemconfig("RECT", fill="white")
+            sorted = True
+            for i in range(len(bars) -1):
+                if bars[i].height > bars[i+1].height:
+                    sorted = False
+                    break
+            root.sortingArea.update()
+        for i in range(len(bars)):  
+            root.sortingArea.itemconfig(f"{bars[i].height / DEFAULT_HEIGHT_MULTIPLIER}", fill="green")
+            root.sortingArea.update()
+            root.after(10)
+        root.after(1000)
+        root.sortingArea.itemconfig("RECT", fill="white")
+        return bars
+
+
+
     
